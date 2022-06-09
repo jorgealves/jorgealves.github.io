7455e21a53d3fbdc58dedf5cba61165022be55f0
aluno <aluno@mac61.local>
2022-06-09 21:58:08 +0100

    Updates

---
+++ precache-manifest.d44ca3ad654063e5e31d620e526fbaeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "05ccdc521d5ced0524eb",
+    "url": "/static/css/main.2212d315.chunk.css"
+  },
+  {
+    "revision": "05ccdc521d5ced0524eb",
+    "url": "/static/js/main.05ccdc52.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "2a24f13e43a0f0455b17",
+    "url": "/static/js/2.2a24f13e.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "6d0a9e947aa7408cd183beb013420fe2",
+    "url": "/static/media/logoDark.6d0a9e94.gif"
+  },
+  {
+    "revision": "116b13c9ebe919f93377c68f5abb1447",
+    "url": "/static/media/belem.116b13c9.mp4"
+  },
+  {
+    "revision": "7d7241ec0aad716622867de891720976",
+    "url": "/index.html"
+  }
+];
