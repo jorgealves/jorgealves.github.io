4ce5de82deb7017bc889fdf64fadc9dfbc0c4d26
Jorge Alves <6217708+jorgealves@users.noreply.github.com>
2026-01-21 18:04:03 +0000

    feat: new website structure

--- Makefile
+++
@@ -1,5 +0,0 @@
-
-gh-deploy:
-	npm i 
-	npm run predeploy
-	npm run deploy
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '﹛📟﹜ Hey! I\'m Jorge Alves';
+export const SITE_DESCRIPTION = 'Jorge Alves - Software Engineer, Mentor';
